3cbe312625400415777606183f938a3e601f3401
Ubuntu <ubuntu@ip-10-139-62-222.ec2.internal>
2014-02-05 02:38:05 +0000

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CaCallowayart::Application.config.secret_key_base = '673e652cc1e0261c29d59398f1fae8470687d602c99e28b49d97385ec209e5071c56eb305f1c27eaad710079d412ca23c127e1ff912d12fbc900fc8abdb938cf'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CaCallowayart::Application.config.session_store :cookie_store, key: '_ca-callowayart_session'
