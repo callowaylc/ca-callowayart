f27c25de3d63f29aa7335e379c27aa2fb8d3ba98
Ubuntu <ubuntu@ip-172-31-44-74.ec2.internal>
2014-06-08 01:31:00 +0000

    A User will not see duplicate images

--- app/controllers/listing_controller.rb
+++ app/controllers/listing_controller.rb
@@ -12,7 +12,6 @@ class ListingController < ApplicationController
 
     # get associated art from the artist
     @listings  = query :collection, tags: [ params[:artist] ]
-    @listings += query :collection_sold, tags: [ params[:artist] ]
 
     @slug     = params[:slug]
     @artist   = params[:artist]
