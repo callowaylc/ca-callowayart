181d86ec892432700eaa17065223de3f19a6c265
Ubuntu <ubuntu@ip-172-31-42-206.ec2.internal>
2014-06-20 02:41:34 +0000

    updated ApplicationHelper#uri_for

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,23 @@
 module ApplicationHelper
   def slugify(string)
-    slug = string.downcase.strip.gsub(' ', '-').gsub(/[^\w-]/, '') if string
-    slug.gsub( /\-{2,}/, '-' )
+    unless string.nil?
+      slug = string.downcase.strip.gsub(' ', '-').gsub(/[^\w-]/, '') if string
+      slug.gsub( /\-{2,}/, '-' )
+    end
   end
 
+  def uri_for(listing)
 
+    # if listing contains an id, then we build a uri for a individual 
+    # listing
+    unless listing[:id].nil?
+      uri  = "/listing"
+      uri += "/#{listing[:artist]}" unless listing[:artist]
+      uri += "/#{listing[:title]}"
+    else
+      request.path + '/' + listing[:slug]      
+    end
+  end
 
   def tags
     # basically moving the chaos that is the current route
--- app/helpers/gallery_helper.rb
+++ app/helpers/gallery_helper.rb
@@ -1,38 +1,5 @@
 module GalleryHelper
 
-	def uri_for listing
-
-    return request.path + '/' + listing[:slug]
-    
-		# get current path and uses slugified
-    # caption property to build new resource
-    begin 
-      unless listing[:count].nil?
-
-        # depending on resource path, return either 
-        # artist slug or exhibit slug
-        if request.path =~ /exhibit/
-          request.path.sub( /\/(past|current|upcoming)/, '' ) + '/' + 
-          listing[:exhibit_slug]
-        else
-          request.path.sub( 'gallery', 'collection' ) + '/' + 
-          listing[:artist_slug]
-        end
-
-        
-
-
-      else
-        '/listing'                + '/' + 
-        slugify(listing[:artist]) + '/' + 
-        slugify(listing[:title])
-      end
-
-    rescue
-      
-    end
-	end
-
   def description_for listing
     if listing[:description]
       # remove first line of description if same as title
