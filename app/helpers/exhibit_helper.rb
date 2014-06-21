d2bc72e5f9bcd512cca17d3757fb429a88316395
Ubuntu <ubuntu@ip-172-31-42-206.ec2.internal>
2014-06-21 22:01:18 +0000

    fixed ExhibitHelper#uri_for for past exhibits

--- app/helpers/exhibit_helper.rb
+++ app/helpers/exhibit_helper.rb
@@ -1,2 +1,13 @@
 module ExhibitHelper
+  def uri_for(listing)
+    if listing[:id]
+      uri  = "/listing"
+      uri += "/#{listing[:artist_slug]}" if listing[:artist_slug]
+      uri += "/#{listing[:slug]}"    
+
+    else
+      "/exhibit/#{listing[:slug]}"
+    end
+
+  end
 end
