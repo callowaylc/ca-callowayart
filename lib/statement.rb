2c4fd1b1ed181db08dbd53e91480c91f943b8a5b
Ubuntu <ubuntu@ip-172-31-42-206.ec2.internal>
2014-07-05 16:59:51 +0000

    stashing

--- lib/statement.rb
+++ lib/statement.rb
@@ -15,86 +15,91 @@ class Statement
 
       # parse and return valid statement and pass to 
       # elasticsearch client
-      statement = erb.result OpenStruct.new(params).instance_eval { 
+      statement = erb.result(OpenStruct.new(params).instance_eval { 
         binding 
-      }
-      result    = client.search index: 'callowayart', 
-                                body:  statement
+      })
 
-      # now massage result set into a simpler data structure
-      data = [ ]
+      if block_given?
+        yield( statement )
 
-      # if aggregations have been returned, we are returning
-      # a grouped result set
-      if result['aggregations'].nil?
-        result['hits']['hits'].each do | hash |
-          # assign 
-          record = hash['_source']
-          record.merge!({
-            slug:  record['title_slug'],
-            image: record['constrainedw'],
-            available: !record['tags'].include?( 'not-available' )
-          })
+      else
+        # now massage result set into a simpler data structure
+        data   = [ ]
+        result = client.search index: 'callowayart', 
+                                  body:  statement
 
-          data << record.with_indifferent_access
-        end
+        # if aggregations have been returned, we are returning
+        # a grouped result set
+        if result['aggregations'].nil?
+          result['hits']['hits'].each do | hash |
+            # assign 
+            record = hash['_source']
+            record.merge!({
+              slug:  record['title_slug'],
+              image: record['constrainedw'],
+              available: !record['tags'].include?( 'not-available' )
+            })
 
-      else                
-        result['aggregations'].first.pop['buckets'].each do | bucket |
-
-          aggregate_value = lambda do | key |
-            begin
-              bucket[key]['buckets'].sample['key']
-            rescue
-              nil
-            end
-          end
-
-          # we don't want collections larger than 150, we check
-          # doc_count and exclude when over max count 
-          # TODO: this should be moved to elasticsearch statement 
-          # but there is currently not a max_doc_count field
-          if bucket['doc_count'] < 150
-            record = { 
-              title: bucket['key'],
-              slug:  bucket['key'],
-              count: bucket['doc_count'],
-              available: true
-            }
-
-            # iterate through bucket fields - the result set will
-            # be the arbiter of what fields are available
-            bucket.keys.each do | key |
-              if ( value = bucket[key] ).kind_of?( Hash )
-                # remove all empty value fields 
-                buckets = value['buckets'].reject do | bucket | 
-                  bucket['key'].nil? || 
-                  bucket['key'].kind_of?( String) && bucket['key'].empty?
-                end
-
-                # now assign sample from buckets with non-null values
-                record[key.to_sym] = buckets.sample['key'] unless buckets.empty?
-
-                # also get reference to collection
-                if buckets.count > 1
-                  record["#{key}_collection"] = buckets.map { | bucket | bucket['key'] }
-                end
-
-    
-              else
-                record[key.to_sym] = value
-              end
-
-
-            end         
-
-            # add record to queue
             data << record.with_indifferent_access
           end
-        end
-      end
 
-      data
+        else                
+          result['aggregations'].first.pop['buckets'].each do | bucket |
+
+            aggregate_value = lambda do | key |
+              begin
+                bucket[key]['buckets'].sample['key']
+              rescue
+                nil
+              end
+            end
+
+            # we don't want collections larger than 150, we check
+            # doc_count and exclude when over max count 
+            # TODO: this should be moved to elasticsearch statement 
+            # but there is currently not a max_doc_count field
+            if bucket['doc_count'] < 150
+              record = { 
+                title: bucket['key'],
+                slug:  bucket['key'],
+                count: bucket['doc_count'],
+                available: true
+              }
+
+              # iterate through bucket fields - the result set will
+              # be the arbiter of what fields are available
+              bucket.keys.each do | key |
+                if ( value = bucket[key] ).kind_of?( Hash )
+                  # remove all empty value fields 
+                  buckets = value['buckets'].reject do | bucket | 
+                    bucket['key'].nil? || 
+                    bucket['key'].kind_of?( String) && bucket['key'].empty?
+                  end
+
+                  # now assign sample from buckets with non-null values
+                  record[key.to_sym] = buckets.sample['key'] unless buckets.empty?
+
+                  # also get reference to collection
+                  if buckets.count > 1
+                    record["#{key}_collection"] = buckets.map { | bucket | bucket['key'] }
+                  end
+
+      
+                else
+                  record[key.to_sym] = value
+                end
+
+
+              end         
+
+              # add record to queue
+              data << record.with_indifferent_access
+            end
+          end
+        end
+
+        data
+      end
     end
 
     def method_missing(name, *arguments)
