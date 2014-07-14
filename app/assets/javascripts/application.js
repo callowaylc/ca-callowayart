0baf56f81f4ee5326cbcb655e061fa8e5a7d15be
Ubuntu <ubuntu@ip-172-31-42-206.ec2.internal>
2014-07-14 03:19:40 +0000

    A user can view breadcrumbs from search and gallery

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,11 +13,8 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
-//= require gallery
-//= require listing
-//= require search
 
-$(document).ready(function() {
+$(document).ready(func = function() {
 
   $('#search').click(search = function() {
     if ((val = $('#q').val())) {
@@ -30,6 +27,52 @@ $(document).ready(function() {
       search()
     }
   })
+
+
+  if (location.pathname.match(/^\/(gallery|search)/)) {
+    localStorage.setItem("gallery-referrer", location.pathname)
+  }
+
+  // TODO: below is a hack to deal with gallery and listings
+  // loading together when turbolinks is enabled
+  if (location.pathname.match( /^\/listing/ )) {
+
+    // reset content in breadcrumbs
+    $('#breadcrumbs').html('')
+
+    referer = localStorage.getItem( 'gallery-referrer' )
+
+    // check if referer is from gallery, if the case, we will
+    // build breadcrumbs from path
+    if (referer && (match = referer.match( /^\/(gallery|search)/ )[1])) {
+
+      // remove all previous breadcrumbs
+      $('#breadcrumbs').html(
+        "<span class='breadcrumb'>/ " + match + "</span>"
+      )
+      // remove gallery from path and seperate out
+      // path nodes
+      resource  = '/' + match + '/'
+      tags      = referer.replace( resource, '' ).split( '/' )
+      container = $("#breadcrumbs") 
+
+      for(counter = 0; counter < tags.length; counter++) {
+        href = resource + tags.slice(0, counter + 1).join( '/' )
+        container.append(
+          " / <span class='breadcrumb' >"      + 
+          "<a href='" + href + "' >"        +
+            tags[counter].replace( '-', ' ') +
+          "</a></span>"
+        )
+      }
+    }
+  }
+
+
+  
+})
+setTimeout(function() {
+  $(window).bind( 'page:change', func )
 })
 
 function getParameterByName(name) {
--- app/assets/javascripts/search.js.coffee
+++ app/assets/javascripts/search.js.coffee
@@ -1,11 +1,11 @@
-$(document).ready func = ->
+$(document).ready sfunc = ->
   # TODO: below is a hack to deal with gallery and listings
   # loading together when turbolinks is enabled
   if location.pathname.match /^\/search/
     # set last pathname, which will be used on listing page
-    localStorage.setItem "gallery-referrer", 
+    localStorage.setItem "gallery-referrer", location.pathname
 
     setTimeout ->
-      $(window).bind "page:change", getParameterByName "q"
-      func = null
+      $(window).bind "page:change", sfunc
+      sfunc = null
       return
