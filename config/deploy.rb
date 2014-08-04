b8dd34e3188ffe5efaaa3a64f6600b493fbf671d
Ubuntu <ubuntu@ip-172-31-42-206.ec2.internal>
2014-08-04 18:31:08 +0000

    stashing

--- config/deploy.rb
+++ config/deploy.rb
@@ -24,76 +24,14 @@ set :deploy_to,     '/releases'
 
 namespace :deploy do
 
-  desc 'Restart application'
-  desc 'Kick varnish'
-  task :restart do
+  desc 'trigger application restart after deployment'
+  after :finished, :application do
     on roles(:web), in: :sequence, wait: 5 do
-      # Your restart mechanism here, for example:
-      # execute :touch, release_path.join('tmp/restart.txt')
-
-      # restart unicorn server
-      # desc 'kicking varnish tires'
-      execute 'sudo service varnish restart'
+      invoke 'application:restart'
     end
-  end
-
-  desc 'Kick unicorn'
-  task :restart do
-    on roles(:web), in: :sequence, wait: 5 do
-      # Your restart mechanism here, for example:
-      # execute :touch, release_path.join('tmp/restart.txt')
-
-      # restart unicorn server
-      # desc 'kicking varnish tires'
-      #execute 'rm -rf tmp/pids/*'
-      #execute 'rails s &'
-    end
-  end
-
-  desc 'Kick nginx'
-  task :restart do
-    on roles(:web), in: :sequence, wait: 5 do
-      # Your restart mechanism here, for example:
-      # execute :touch, release_path.join('tmp/restart.txt')
-
-      # restart unicorn server
-      # desc 'kicking varnish tires'
-      execute 'sudo service nginx restart'
-    end
-  end  
-
-  desc "Check that we can access everything"
-  task :check_write_permissions do
-    on roles(:all) do |host|
-      if test("[ -w #{fetch(:deploy_to)} ]")
-        info "#{fetch(:deploy_to)} is writable on #{host}"
-      else
-        error "#{fetch(:deploy_to)} is not writable on #{host}"
-      end
-    end
-  end  
-
-  after :publishing, :restart
-
-  after :restart, :clear_cache do
-    #on roles(:web), in: :groups, limit: 3, wait: 10 do
-    on roles(:web), in: :sequence, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
-      root  = '/app/callowyart'
-      execute "ln -s ~/Develop/ca-callowayart/tmp #{root}/tmp"
-
-      begin
-        execute "cat tmp/pids/server.pid`; rails server -d -p 8080"
-      rescue 
-        execute "cd #{root} && nohup rails s &"
-      end
-    end
-  end
-
+  end     
 end
+
 
 # Uncomment these lines to additionally create your upload and cache
 # directories in the shared location when running `deploy:setup`.
---
+++ lib/capistrano/tasks/application.rake
@@ -0,0 +1,59 @@
+# author:      christian calloway callowaylc@gmail.com
+# description: Defines capistrano tasks for application restart
+# usage:       cap staging application:restart
+#              
+
+
+# application namespace definition
+#####################################
+
+namespace :application do
+  desc <<-eod 
+    Restart all application dependencies
+  eod
+  task :restart do
+    on roles(:web), in: :sequence, wait: 5 do
+      # restart all external services; order is important
+      # here
+      invoke 'unicorn:restart'
+      invoke 'nginx:restart'
+      invoke 'unicorn:restart'
+    end
+  end  
+end
+
+namespace :varnish do
+  desc <<-eod 
+    Restart transparent cache layer
+  eod
+  task :restart do
+    on roles(:web), in: :sequence, wait: 5 do
+      capture :sudo, 'service varnish restart'
+    end
+  end 
+end
+
+namespace :nginx do
+  desc <<-eod 
+    Restart web server
+  eod
+  task :restart do
+    on roles(:web), in: :sequence, wait: 5 do
+      capture :sudo, 'service nginx restart'
+    end
+  end 
+end
+
+namespace :unicorn do
+  desc <<-eod 
+    Restart rails server
+  eod
+  task :restart do
+    on roles(:web), in: :sequence, wait: 5 do
+      within release_path do
+        run './run/unicorn'
+      end
+    end
+  end 
+end
+
